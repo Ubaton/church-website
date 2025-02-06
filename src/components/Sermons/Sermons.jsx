02b8ea4714f08c79b4bb7eac8e03491ee7cc36ae
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-02-06 21:32:31 +0200

    Time formatting

--- src/components/Sermons/Sermons.jsx
+++ src/components/Sermons/Sermons.jsx
@@ -9,8 +9,15 @@ import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { Slider } from "@/components/ui/slider";
 
 const formatTime = (time) => {
-  const minutes = Math.floor(time / 60);
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
   const seconds = Math.floor(time % 60);
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  }
   return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 };
 
